3600aa33f0ae6934a5f110a3ec57681bf24e2e15
Gábor <deutschgabor92@gmail.com>
2023-08-25 15:50:26 +0200

    spa changed

--- tzs_backend/resources/views/spa.blade.php
+++ tzs_backend/resources/views/spa.blade.php
@@ -19,12 +19,9 @@ $config = [
 <body>
   <div id="app"></div>
 
-  {{-- Global configuration object --}}
   <script>
     window.config = @json($config);
   </script>
-
-  {{-- Load the application scripts --}}
   
   <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
   <script src="{{ mix('dist/js/app.js') }}"></script>
